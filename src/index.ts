7ec10f9fd47b4f13013ecdc0215c5c3a8833e08c
高椋 貴大 <takahiro.takamuku@vrg.jp>
2020-07-06 12:57:38 +0900

    Add server boot

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
-const hello = (name: string): void => console.log('hello, ', name);
-hello('LINE Wors Travel!!!')
+import express from 'express'
+
+const app: express.Express = express();
+
+// CORS Permission
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Routing test
+const router: express.Router = express.Router()
+router.get('/api/getTest', (req:express.Request, res:express.Response) => {
+  res.send(req.query)
+})
+router.post('/api/postTest', (req:express.Request, res:express.Response) => {
+  res.send(req.body)
+})
+app.use(router)
+
+// Boot server on 3000 port
+app.listen(3000,()=>{ console.log('Example app listening on port 3000!') })
